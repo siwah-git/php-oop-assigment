6f9f69f35276ccb8d7df6156052709227e18a964
Restiidr <restysebelasjanuari@github.com>
2025-08-21 00:58:42 +0700

    turuu ges

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 
 // === Autoload all classes from the specified folders ===
 spl_autoload_register(function ($class) {
-    $folders = ['classes/basics', 'classes/advanced', 'classes/oop', 'classes/databases'];
+    $folders = ['classes/basics', 'classes/advanced', 'classes/oop', 'classes/database'];
     foreach ($folders as $folder) {
         $file = $folder . '/' . $class . '.php';
         if (file_exists($file)) {
@@ -21,31 +21,34 @@ spl_autoload_register(function ($class) {
 });
 
 // === Class call examples ===
-echo "=== Prime Generator ===" . PHP_EOL;
+echo "=== Prime Generator ===" . PHP_EOL . "<br>";
 $prime = new PrimeGenerator(20);
+echo "<pre>";
 print_r($prime->generate());
-echo PHP_EOL;
+echo "</pre>" . PHP_EOL . "<br>";
 
-echo "=== CSV Reader ===" . PHP_EOL;
+echo "=== CSV Reader ===" . PHP_EOL . "<br>";
 $csv = new CsvReader('data/students.csv');
+echo "<pre>";
 print_r($csv->getHeader());
 print_r($csv->getRows());
-echo PHP_EOL;
+echo "</pre>" . PHP_EOL . "<br>";
 
-echo "=== Logger System ===" . PHP_EOL;
+echo "=== Logger System ===" . PHP_EOL . "<br>";
 $fileLogger = new FileLogger('data/logs.txt');
 $appFile = new App($fileLogger);
 $appFile->run('Application started.');
+echo "<br>";
 
 $consoleLogger = new ConsoleLogger();
 $appConsole = new App($consoleLogger);
 $appConsole->run('Application is running.');
-echo PHP_EOL;
+echo "<br>";
 
-echo "=== Order Management ===" . PHP_EOL;
+echo "=== Order Management ===" . PHP_EOL . "<br>";
 $orderRepo = new OrderRepository();
 $orderRepo->createOrder("Resty", 150000);
 $orderRepo->createOrder("Siwah", 200000);
 $orderRepo->createOrder("Sinta", 300000);
-echo "Total Orders: " . $orderRepo->getTotalOrders() . PHP_EOL;
-echo "Average Order Value: Rp " . number_format($orderRepo->getAverageOrderValue(), 0, ',', '.') . PHP_EOL;
+echo "Total Orders: " . $orderRepo->getTotalOrders() . PHP_EOL . "<br>";
+echo "Average Order Value: Rp " . number_format($orderRepo->getAverageOrderValue(), 0, ',', '.') . PHP_EOL . "<br>";
