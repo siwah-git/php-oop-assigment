c8fdf272dcf2aae56244ae8c6b3bc34b6752d52b
Sinta Febriyanti <sinta34.cmd@gmail.com>
2025-08-21 12:19:48 +0700

    siang

--- classes/oop/Triangle.php
+++ classes/oop/Triangle.php
@@ -1,17 +1,30 @@
 <?php
 
-require_once "PlannerShapes.php";
-
+/**
+ * class Triangle to calculate the area of ​​a triangular shape
+ * @var float height of the triangle.
+ * @var float base length of the triangle.
+ * 
+ */
 class Triangle extends PlannerShapes{
     private $base;
     private $height;
 
+    /**
+     * Constructs a new Triangle instance.
+     * 
+     * @param base $base of the triangle
+     * @param height $height of the tiangle 
+     */
 
     public function __construct($base, $height){
         $this->base = $base;
         $this->height = $height;
     } 
 
+    /**
+     *  Calculates and returns the area of ​​the triangle
+     */
     public function getArea(){
         return 0.5 * $this->height * $this->base;
     }
